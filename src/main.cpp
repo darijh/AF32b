dc1c64e15b49e9eaeb1dd762a092a197a799f467
PC-Dario <dariohernandez@selecsrl.com>
2025-04-10 10:52:22 -0300

    duty y libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
 #include <Arduino.h>
+#include <EEPROM.h>
+
 #include <hardware.h>
 #include <regmap.h>
 
-void setup()
-{
+uint16_t duty(uint8_t duty) { return map(duty, 0, 100, 0, 4095); }
+
+void setup() {
+  analogReadResolution(12);
   analogWriteResolution(12);
-  pinMode(HW_STS, OUTPUT);
+  pinMode(HW_ID_B0, INPUT_PULLUP);
+  pinMode(HW_ID_B1, INPUT_PULLUP);
+  pinMode(HW_ID_B2, INPUT_PULLUP);
+  pinMode(HW_ID_B3, INPUT_PULLUP);
+  pinMode(HW_CTRL, INPUT_PULLUP);
   pinMode(HW_SPV_A, OUTPUT);
   pinMode(HW_SPI_A, OUTPUT);
   pinMode(HW_SPV_B, OUTPUT);
   pinMode(HW_SPI_B, OUTPUT);
+  pinMode(HW_STS, OUTPUT);
   pinMode(HW_OK, OUTPUT);
   pinMode(HW_FAIL, OUTPUT);
   DBG_PORT.begin(115200);
-  while (!DBG_PORT)
-  {
-  }
+  delay(5000);
+  analogWrite(HW_SPV_A, duty(20));
+  analogWrite(HW_SPI_A, duty(40));
+  analogWrite(HW_SPV_B, duty(60));
+  analogWrite(HW_SPI_B, duty(80));
 }
 
 void loop() {}
