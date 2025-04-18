cf953057151700f533cb95647cd782d6745c4f1b
PC_Casa <darijh@hotmail.com>
2025-04-18 19:25:00 -0300

    formato

--- lib/PID_Library/PID.cpp
+++ lib/PID_Library/PID.cpp
@@ -4,25 +4,31 @@ PID::PID(double kp, double ki, double kd)
     : _kp(kp), _ki(ki), _kd(kd), _integral(0), _prevError(0),
       _outputMin(0), _outputMax(10000), _firstCompute(true) {}
 
-void PID::setOutputLimits(double min, double max) {
+void PID::setOutputLimits(double min, double max)
+{
     _outputMin = min;
     _outputMax = max;
 }
 
-void PID::reset() {
+void PID::reset()
+{
     _integral = 0;
     _prevError = 0;
     _firstCompute = true;
 }
 
-double PID::compute(double input, double setpoint) {
+double PID::compute(double input, double setpoint)
+{
     double error = setpoint - input;
     _integral += error;
 
     double derivative = 0;
-    if (!_firstCompute) {
+    if (!_firstCompute)
+    {
         derivative = error - _prevError;
-    } else {
+    }
+    else
+    {
         _firstCompute = false;
     }
 
@@ -30,8 +36,10 @@ double PID::compute(double input, double setpoint) {
 
     double output = _kp * error + _ki * _integral + _kd * derivative;
 
-    if (output > _outputMax) output = _outputMax;
-    else if (output < _outputMin) output = _outputMin;
+    if (output > _outputMax)
+        output = _outputMax;
+    else if (output < _outputMin)
+        output = _outputMin;
 
     return output;
 }
